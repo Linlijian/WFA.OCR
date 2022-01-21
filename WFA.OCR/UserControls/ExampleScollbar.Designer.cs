edfda9f88925f42526c8f32a17ba545db2083e3c
Lintemi <prawmng@hotmail.co.th>
2022-01-21 17:06:04 +0700

    [UPDATE] Plugin scollbar

---
+++ WFA.OCR/UserControls/ExampleScollbar.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WFA.OCR.UserControls
+{
+	partial class ExampleScollbar
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UCClientBody));
+			this.panelItem = new System.Windows.Forms.Panel();
+			this.btnGroupImage = new System.Windows.Forms.Button();
+			this.btnSetting = new System.Windows.Forms.Button();
+			this.btnImagetoPDF = new System.Windows.Forms.Button();
+			this.btnPDFtoImage = new System.Windows.Forms.Button();
+			this.panelHideScollbar = new System.Windows.Forms.Panel();
+			this.ucScrollbar = new CustomControls.UCScrollbar();
+			this.panelItem.SuspendLayout();
+			this.panelHideScollbar.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panelItem
+			// 
+			this.panelItem.AutoScroll = true;
+			this.panelItem.Controls.Add(this.btnGroupImage);
+			this.panelItem.Controls.Add(this.btnSetting);
+			this.panelItem.Controls.Add(this.btnImagetoPDF);
+			this.panelItem.Controls.Add(this.btnPDFtoImage);
+			this.panelItem.Location = new System.Drawing.Point(0, 0);
+			this.panelItem.Name = "panelItem";
+			this.panelItem.Size = new System.Drawing.Size(900, 560);
+			this.panelItem.TabIndex = 4;
+			// 
+			// btnGroupImage
+			// 
+			this.btnGroupImage.FlatAppearance.BorderSize = 0;
+			this.btnGroupImage.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
+			this.btnGroupImage.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+			this.btnGroupImage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnGroupImage.Font = new System.Drawing.Font("Itim", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnGroupImage.ForeColor = System.Drawing.Color.White;
+			this.btnGroupImage.Image = ((System.Drawing.Image)(resources.GetObject("btnGroupImage.Image")));
+			this.btnGroupImage.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.btnGroupImage.Location = new System.Drawing.Point(581, 185);
+			this.btnGroupImage.Name = "btnGroupImage";
+			this.btnGroupImage.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+			this.btnGroupImage.Size = new System.Drawing.Size(240, 80);
+			this.btnGroupImage.TabIndex = 7;
+			this.btnGroupImage.Text = "Group Image";
+			this.btnGroupImage.UseVisualStyleBackColor = true;
+			// 
+			// btnSetting
+			// 
+			this.btnSetting.FlatAppearance.BorderSize = 0;
+			this.btnSetting.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
+			this.btnSetting.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+			this.btnSetting.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnSetting.Font = new System.Drawing.Font("Itim", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnSetting.ForeColor = System.Drawing.Color.White;
+			this.btnSetting.Image = ((System.Drawing.Image)(resources.GetObject("btnSetting.Image")));
+			this.btnSetting.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.btnSetting.Location = new System.Drawing.Point(39, 295);
+			this.btnSetting.Name = "btnSetting";
+			this.btnSetting.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+			this.btnSetting.Size = new System.Drawing.Size(240, 80);
+			this.btnSetting.TabIndex = 6;
+			this.btnSetting.Text = "Setting";
+			this.btnSetting.UseVisualStyleBackColor = true;
+			// 
+			// btnImagetoPDF
+			// 
+			this.btnImagetoPDF.FlatAppearance.BorderSize = 0;
+			this.btnImagetoPDF.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
+			this.btnImagetoPDF.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+			this.btnImagetoPDF.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnImagetoPDF.Font = new System.Drawing.Font("Itim", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnImagetoPDF.ForeColor = System.Drawing.Color.White;
+			this.btnImagetoPDF.Image = ((System.Drawing.Image)(resources.GetObject("btnImagetoPDF.Image")));
+			this.btnImagetoPDF.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.btnImagetoPDF.Location = new System.Drawing.Point(311, 185);
+			this.btnImagetoPDF.Name = "btnImagetoPDF";
+			this.btnImagetoPDF.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+			this.btnImagetoPDF.Size = new System.Drawing.Size(240, 80);
+			this.btnImagetoPDF.TabIndex = 5;
+			this.btnImagetoPDF.Text = "Image to PDF";
+			this.btnImagetoPDF.UseVisualStyleBackColor = true;
+			// 
+			// btnPDFtoImage
+			// 
+			this.btnPDFtoImage.FlatAppearance.BorderSize = 0;
+			this.btnPDFtoImage.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
+			this.btnPDFtoImage.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+			this.btnPDFtoImage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnPDFtoImage.Font = new System.Drawing.Font("Itim", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnPDFtoImage.ForeColor = System.Drawing.Color.White;
+			this.btnPDFtoImage.Image = ((System.Drawing.Image)(resources.GetObject("btnPDFtoImage.Image")));
+			this.btnPDFtoImage.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.btnPDFtoImage.Location = new System.Drawing.Point(39, 185);
+			this.btnPDFtoImage.Name = "btnPDFtoImage";
+			this.btnPDFtoImage.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+			this.btnPDFtoImage.Size = new System.Drawing.Size(240, 80);
+			this.btnPDFtoImage.TabIndex = 4;
+			this.btnPDFtoImage.Text = "Exam";
+			this.btnPDFtoImage.UseVisualStyleBackColor = true;
+			// 
+			// panelHideScollbar
+			// 
+			this.panelHideScollbar.Controls.Add(this.ucScrollbar);
+			this.panelHideScollbar.Location = new System.Drawing.Point(880, 0);
+			this.panelHideScollbar.Name = "panelHideScollbar";
+			this.panelHideScollbar.Size = new System.Drawing.Size(20, 560);
+			this.panelHideScollbar.TabIndex = 5;
+			// 
+			// ucScrollbar
+			// 
+			this.ucScrollbar.ChannelColor = System.Drawing.Color.Black;
+			this.ucScrollbar.DownArrowImage = ((System.Drawing.Image)(resources.GetObject("ucScrollbar.DownArrowImage")));
+			this.ucScrollbar.LargeChange = 10;
+			this.ucScrollbar.Location = new System.Drawing.Point(0, 3);
+			this.ucScrollbar.Maximum = 100;
+			this.ucScrollbar.Minimum = 0;
+			this.ucScrollbar.MinimumSize = new System.Drawing.Size(15, 92);
+			this.ucScrollbar.Name = "ucScrollbar";
+			this.ucScrollbar.Size = new System.Drawing.Size(17, 554);
+			this.ucScrollbar.SmallChange = 1;
+			this.ucScrollbar.TabIndex = 9;
+			this.ucScrollbar.ThumbBottomImage = ((System.Drawing.Image)(resources.GetObject("ucScrollbar.ThumbBottomImage")));
+			this.ucScrollbar.ThumbBottomSpanImage = ((System.Drawing.Image)(resources.GetObject("ucScrollbar.ThumbBottomSpanImage")));
+			this.ucScrollbar.ThumbMiddleImage = ((System.Drawing.Image)(resources.GetObject("ucScrollbar.ThumbMiddleImage")));
+			this.ucScrollbar.ThumbTopImage = ((System.Drawing.Image)(resources.GetObject("ucScrollbar.ThumbTopImage")));
+			this.ucScrollbar.ThumbTopSpanImage = ((System.Drawing.Image)(resources.GetObject("ucScrollbar.ThumbTopSpanImage")));
+			this.ucScrollbar.UpArrowImage = ((System.Drawing.Image)(resources.GetObject("ucScrollbar.UpArrowImage")));
+			this.ucScrollbar.Value = 0;
+			// 
+			// UCClientBody
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.Controls.Add(this.panelHideScollbar);
+			this.Controls.Add(this.panelItem);
+			this.Name = "UCClientBody";
+			this.Size = new System.Drawing.Size(900, 560);
+			this.panelItem.ResumeLayout(false);
+			this.panelHideScollbar.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Panel panelItem;
+		private System.Windows.Forms.Panel panelHideScollbar;
+		private System.Windows.Forms.Button btnGroupImage;
+		private System.Windows.Forms.Button btnSetting;
+		private System.Windows.Forms.Button btnImagetoPDF;
+		private System.Windows.Forms.Button btnPDFtoImage;
+		private CustomControls.UCScrollbar ucScrollbar;
+	}
+}
---
+++ WFA.OCR/UserControls/ExampleScollbar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Windows.Forms;
+using Helper;
+
+namespace WFA.OCR.UserControls
+{
+    public partial class ExampleScollbar : UserControl
+    {
+		public ExampleScollbar()
+        {
+            InitializeComponent();
+		}
+
+		#region event
+		#endregion
+		
+		private void btnexam_Click(object sender, EventArgs e)
+        {
+            if (!MainForm.Instance.PnlClientBody.Controls.ContainsKey("UCClientExam"))
+            {
+                UserControlHelper.SetUserControl(MainForm.Instance.PnlClientBody, userControl: new UCClientExam(), dockStyle: UserControlDockStyle.DockStyleFill);
+            }
+            MainForm.Instance.PnlClientBody.Controls["UCClientExam"].BringToFront();
+            MainForm.Instance.PnlTitle.BackColor = Color.FromArgb(32, 32, 32);
+        }
+	}
+}
