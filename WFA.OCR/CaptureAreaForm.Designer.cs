2928586f8b0b915c4340abfd4a246cd94b42d5e9
Lintemi <prawmng@hotmail.co.th>
2022-02-04 18:41:19 +0700

    [UPDATE] Add CaptureAreaForm

---
+++ WFA.OCR/CaptureAreaForm.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace WFA.OCR
+{
+	partial class CaptureAreaForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Text = "CaptureAreaForm";
+
+			this.SuspendLayout();
+
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+			this.ClientSize = new System.Drawing.Size(200, 240);
+			this.ControlBox = false;
+			this.Cursor = System.Windows.Forms.Cursors.Arrow;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "CaptureAreaForm";
+			this.Opacity = 0.1;
+			this.TransparencyKey = System.Drawing.Color.White;
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.ResumeLayout(false);
+		}
+
+		#endregion
+	}
+}
